f3c6245045d5cc9b828132df2de3cad50fdc8151
Lin Rui <linrui143.gmail.com>
2016-06-16 22:09:15 +0000

    Edit Model Belongs_To and Has_Many, and Game STI Edit Forgot model pieces belong_to users. Deleted extra line from STI tutorial that was not relevant.

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,7 +2,4 @@ class Piece < ActiveRecord::Base
   belongs_to :users
   belongs_to :games
 
-  def self.types
-    %w(Pawn Bishop Knight Rook Queen King)
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   has_many :games
+  has_many :pieces
 end
