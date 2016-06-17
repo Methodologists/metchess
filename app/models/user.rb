9161da871342f86475ef82631088903c43c64bb2
Lin Rui <linrui143.gmail.com>
2016-06-17 00:37:30 +0000

    Fix Merge Conflict On User and Schema after merge #5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,8 @@
 class User < ActiveRecord::Base
-<<<<<<< HEAD
   has_many :games
   has_many :pieces
-=======
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
->>>>>>> master
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20160616024120) do
 
-=======
-ActiveRecord::Schema.define(version: 20160613171425) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
