c6052a6f50d4224076752519a46d1921637d6810
Aleks Real <aleksanderreal@gmail.com>
2016-06-21 04:47:13 +0000

    Fixed database issue removed one of the users tables from database

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,14 @@
 class GamesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :initialize_board!
+
+  def create
+    game = Game.create(game_params)
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permits(:play_white_id, :player_black_id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -36,14 +36,6 @@ ActiveRecord::Schema.define(version: 20160616024120) do
   end
 
   create_table "users", force: true do |t|
-    t.integer  "user_id"
-    t.string   "email"
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -56,6 +48,7 @@ ActiveRecord::Schema.define(version: 20160616024120) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
