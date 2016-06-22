42bca8c9f5dce1a7468f87cbbbd4a879c18b9445
Lindy Sinclair <lindyrp@gmail.com>
2016-06-22 20:20:36 +0000

    Add board visual files

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,14 +1,24 @@
 class GamesController < ApplicationController
   before_action :authenticate_user!
-  before_action :initialize_board!
+  #before_action :initialize_board!
+
+  def new
+    @game = Game.new
+  end
 
   def create
     game = Game.create(game_params)
+    redirect_to game_path(@game)
   end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
 
   private
 
   def game_params
-    params.require(:game).permits(:play_white_id, :player_black_id)
+    params.require(:game).permit(:player_white_id, :player_black_id)
   end
 end
