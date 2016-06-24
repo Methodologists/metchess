b329834d9c2596258bad44499ab7e0858076ec1a
Aleks Real <aleksanderreal@gmail.com>
2016-06-24 01:28:07 +0000

    Pseudo code for valid_move? method

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,3 +1,27 @@
 class King < Piece
 
+  def valid_move?
+    #find king's original location
+    #find king's new location
+    #if/else statement that checks if piece moves within its movable parameters
+      #define the king's parameters for conditional statement of if/else statement (for each of these set to a variable)
+        #move up one: y_cord + 1 
+        #move down one: y_cord - 1
+        #move right one: x_cord + 1
+        #move left one: x_cord - 1
+        #diagonals:
+          #upper right: x_cord + 1, y_cord + 1
+          #upper left: x_cord - 1, y_cord + 1
+          #lower right: x_cord + 1, y_cord - 1
+          #lower left: x_cord - 1, y_cord - 1
+        #for each of these use position_exist? to verify the position exists
+      #if the new location fits the above criteria, then return true. Else return false
+  end
+
+  def position_exist?
+    #if/else statement that checks if position exists
+      #if x_cord && y_cord == negative value return false
+      #if x_cord > 7 && y_cord > 7 return false
+      #else return true
+  end
 end
