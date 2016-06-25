d35abe411225e6cae4c1892aaa36daac628eeb1c
Aleks Real <aleksanderreal@gmail.com>
2016-06-25 20:08:37 +0000

    Still a WIP, worked only in King model

--- app/models/king.rb
+++ app/models/king.rb
@@ -2,8 +2,7 @@ class King < Piece
 
   def valid_move?
     game_loc
-    #find king's original location
-    #find king's new location
+    current_location
     #if/else statement that checks if piece moves within its movable parameters
       #define the king's parameters for conditional statement of if/else statement (for each of these set to a variable)
         #move up one: y_cord + 1 
@@ -19,23 +18,35 @@ class King < Piece
       #if the new location fits the above criteria, then return true. Else return false
   end
 
-  def position_exist?
-    #if/else statement that checks if position exists
-      #if x_cord && y_cord == negative value return false
-      #if x_cord > 7 && y_cord > 7 return false
-      #else return true
+  def position_exist?(position)
+    #checks if position exists
+    if position[0] < 0 && position[1] < 0 && position[0] > 7 && position[1] > 7
+      return false
+    else
+      return true
+    end
   end
 
-  def orig_position
+  def current_location
     #finds the king's original location
-
-  end
-
-  def new_position
-
+    position = [@current_game.king.x_cord, @current_game.king.y_cord]
+    return position
   end
 
   def game_loc
-    return @game
+    #find current game's game_id
+    @current_game = @game.game_id
+    return @current_game
+  end
+
+  def king_positions
+    right_one = [current_location[0] + 1, current_location[1]]
+    left_one = [current_location[0] - 1, current_location[1]]
+    up_one = [current_location[0], current_location[1] + 1]
+    down_one = [current_location[0], current_location[1] - 1]
+    upper_right = [current_location[0] + 1, current_location[1] + 1]
+    upper_left = [current_location[0] - 1, current_location[1] + 1]
+    lower_right = [current_location[0] + 1, current_location[1] - 1]
+    lower_left = [current_location[0] - 1, current_location[1] - 1]
   end
 end
