ac7265d7c76ff1c7b92228d4cf8474a74dde6fcb
Lindy Sinclair <lindyrp@gmail.com>
2016-06-30 00:48:49 +0000

    Start implementing move logic

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,6 +1,11 @@
 class Piece < ActiveRecord::Base
   belongs_to :game
 
+  def move_piece(new_x, new_y)
+    # Add logic to check for validity
+    update_attributes(x_cord: new_x, y_cord: new_y)
+  end
+
   def is_obstructed?
     return false
   end
--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 
 RSpec.describe PiecesController, type: :controller do
 
+  let(:game){Game.create}
+  let(:piece){Pawn.create(x_cord: 0, y_cord: 1, game_id: game.id)}
+
+  describe '#update' do
+    it "should redirect after update" do
+      put :update, game_id: game.id, id: piece.id, piece: {x_cord: 0, y_cord: 2}
+      expect(response).to have_http_status(:redirect)
+    end
+
+    it "should update piece location" do
+      put :update, game_id: game.id, id: piece.id, piece: {x_cord: 0, y_cord: 2}
+      piece.reload
+      expect(piece.x_cord).to eq(0)
+      expect(piece.y_cord).to eq(2)
+    end
+  end
+
+
+
+
 end
