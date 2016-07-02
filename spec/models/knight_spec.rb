77bd13756c4adb9161631251bdb12c6fcdfcb778
Lin Rui <linrui143.gmail.com>
2016-07-02 17:05:30 +0000

    Edit comments in tests Makes more clear where starting points are for tests

--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -5,13 +5,14 @@ RSpec.describe Knight, type: :model do
   describe "#valid_move?" do
 
       {
-        #edge of board
+        #starting at edge of board
         [[0, 0], [1, 2]] => true,
         [[0, 0], [1, -2]] => false, # off-board
         [[0, 0], [2, 1]] => true, 
         [[0, 0], [2, -1]] => false, # off-board
+        [[0, 0], [0, 0]] => false, # no move, wrong logic
         
-        #center of board
+        #starting at center of board
         [[3, 3], [1, 2]] => true,
         [[3, 3], [1, 4]] => true,
         [[3, 3], [5, 2]] => true,
@@ -20,13 +21,9 @@ RSpec.describe Knight, type: :model do
         [[3, 3], [4, 5]] => true,
         [[3, 3], [2, 1]] => true,
         [[3, 3], [2, 5]] => true,
-
-        #other
         [[3, 3], [1, 1]] => false, # wrong logic
         [[3, 3], [0, 0]] => false, # wrong logic
-        [[3, 3], [-1, -1]] => false, # off-board
-        [[0, 0], [0, 0]] => false, # no move
-
+        [[3, 3], [-1, -1]] => false, # wrong logic & off-board
       }.each do |before_and_after_coords, result|
         it "returns #{result} when moving from #{before_and_after_coords.first} to #{before_and_after_coords.last}" do
           knight = Knight.create(x_cord: before_and_after_coords.first.first, y_cord: before_and_after_coords.first.last)
