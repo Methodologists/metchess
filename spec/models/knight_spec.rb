82de771255054ee0ea11e0578331f359b448769b
Lin Rui <linrui143.gmail.com>
2016-07-02 16:59:21 +0000

    Edit tests Changed test methodology to use old and new coordinates instead of testing situationally, so can test more with less lines

--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -4,51 +4,34 @@ RSpec.describe Knight, type: :model do
 
   describe "#valid_move?" do
 
-    let(:knight) { Knight.create(x_cord: starting_x, y_cord: starting_y) }
+      {
+        #edge of board
+        [[0, 0], [1, 2]] => true,
+        [[0, 0], [1, -2]] => false, # off-board
+        [[0, 0], [2, 1]] => true, 
+        [[0, 0], [2, -1]] => false, # off-board
+        
+        #center of board
+        [[3, 3], [1, 2]] => true,
+        [[3, 3], [1, 4]] => true,
+        [[3, 3], [5, 2]] => true,
+        [[3, 3], [5, 4]] => true,
+        [[3, 3], [4, 1]] => true,
+        [[3, 3], [4, 5]] => true,
+        [[3, 3], [2, 1]] => true,
+        [[3, 3], [2, 5]] => true,
 
-    {
-      [-1, -1] => false,
-      [0, 8] => false,
-    }.each do |coords, result|
-      it "returns #{result} when given #{coords}" do
-        expect(knight.valid_move?(*coords)).to eq result
+        #other
+        [[3, 3], [1, 1]] => false, # wrong logic
+        [[3, 3], [0, 0]] => false, # wrong logic
+        [[3, 3], [-1, -1]] => false, # off-board
+        [[0, 0], [0, 0]] => false, # no move
+
+      }.each do |before_and_after_coords, result|
+        it "returns #{result} when moving from #{before_and_after_coords.first} to #{before_and_after_coords.last}" do
+          knight = Knight.create(x_cord: before_and_after_coords.first.first, y_cord: before_and_after_coords.first.last)
+          expect(knight.valid_move?(before_and_after_coords.last.first, before_and_after_coords.last.last)).to eq result
+        end
       end
-    end
-
-    {
-      [1, 2] => true,
-      [2, 1] => true,
-      [1, 1] => false,
-    }.each do |change_in_coords, result|
-      it "returns #{result} when coordinates change by #{change_in_coords}" do
-        expect(knight.valid_move?(
-          knight.x_cord + change_in_coords.first,
-          knight.y_cord + change_in_coords.last
-        )).to eq result
-      end
-    end
-
-
-    [
-      [1, -2],
-      [-1, -2],
-    ].each do |change_in_coords|
-      it "returns false when given #{change_in_coords} because coordinates are off-board" do
-        expect(knight.valid_move?(
-          knight.x_cord + change_in_coords.first,
-          knight.y_cord + change_in_coords.last
-        )).to eq false
-      end
-    end
-
-    # {
-    #   [[0, 0], [5, 3]] => true,
-    #   [[0, 0], [6, 2]] => true,
-    #   [[0, 0], [5, 2]] => false,
-
-    #   [[3, 3], [2, 1]] => true,
-    # }.each do |before_and_after_coords, result|
-    #   it "returns #{result} when moving from #{before_and_after_coords.first} to #{before_and_after_coords.last}"
-    # end
   end
 end
