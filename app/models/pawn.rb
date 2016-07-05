38921c662c9b700e83c0a0379ca2214b476f1b30
Aleks Real <aleksanderreal@gmail.com>
2016-07-05 22:14:09 +0000

    Added pseudo code and some tests, still a WIP

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,5 +1,44 @@
 class Pawn < Piece
 
+  def valid_move?(new_x, new_y)
+    on_board?(new_x, new_y) && passes_pawn_rules?(new_x, new_y)
+  end
+
+  #we need to create a method that makes sure the pawn moves according to its rules
+  def passes_pawn_rules?(x, y)
+
+  end
+
+  #we need to create a method that makes sure the pawn is moving into a position that exists
+  def on_board?(x, y)
+    x >= 0 && x <= 7 && y >=0 && y <= 7
+  end
+
+  #we need to check if its the first move for the pawn
+  def first_move_position?
+    self.y_cord == 1
+  end
+
+  #we need to check whether or not a diagonal move is allowed
+  #this will be incorporated into passes_pawn_rules? method
+
+  def diagonal_allowed?
+
+  end
+
+  #diagonal_allowed? should also check the color of opposition's piece color, if the piece color
+  #is the opposite color then return false
+
+  def same_color?(x, y)
+
+  end
+
+  def two_jumps_allowed?(x, y)
+
+  end
+
+  #should create a separate method that checks that pawn moves 
+
   def image
     if color == "white"
       "\u2659"
--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Pawn, type: :model do
-  
+  describe "valid_move? action" do
+    it "should check that position it's moving to exists" do
+      p = Pawn.create(x_cord: 7, y_cord: 7)
+      expect(p.valid_move?(7, 8)).to eq(false)
+    end
+
+    it "should check that Pawn is allowed to move diagonally" do
+      p = Pawn.create(x_cord: 0, y_cord: 1, color: 'black')
+      o = Pawn.create(x_cord: 1, y_cord: 2, color: 'white')
+      expect(p.valid_move?(1, 2)).to eq(true)
+    end
+
+    it "should check that Pawn moves 2 squares on its first move position" do
+      p = Pawn.create(x_cord: 0, y_cord: 1)
+      expect(p.valid_move?(0, 3)).to eq(true)
+    end
+
+    it "should check that Pawn doesn't move more than 2 squares when on its first move position" do
+      p = Pawn.create(x_cord: 0, y_cord: 1)
+      expect(p.valid_move?(0, 4)).to eq(false)
+    end
+
+    it "should check that Pawn doesn't move 2 squares when it's not on first move position" do
+      p = Pawn.create(x_cord: 0, y_cord: 2)
+      expect(p.valid_move?(0, 4)).to eq(false)
+    end
+  end
 end
