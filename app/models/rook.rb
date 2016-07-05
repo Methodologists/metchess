22c00c93e7398309e0920a3d6b9336d56f989e91
Aleks Real <aleksanderreal@gmail.com>
2016-07-05 22:57:18 +0000

    Added valid_move? method and extra tests

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,5 +1,21 @@
 class Rook < Piece
 
+  def valid_move?(new_x, new_y)
+    position_exist?(new_x, new_y) && passes_rook_rules?(new_x, new_y)
+  end
+
+  def position_exist?(x, y)
+    x >= 0 && x <= 7 && y >= 0 && y <= 7
+  end
+
+  def passes_rook_rules?(x, y)
+    if self.x_cord == x || self.y_cord == y
+      return true
+    else
+      return false
+    end
+  end
+
   def image
     if color == "white"
       "\u2656"
--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe Rook, type: :model do
       expect(r.valid_move?(-5, 0)).to eq(false)
     end
 
-    it "should check that rook moves horizontally" do
+    it "should check that rook moves horizontally to the right" do
       r = Rook.create(x_cord: 0, y_cord: 0)
       expect(r.valid_move?(7, 0)).to eq(true)
     end
 
-    it "should check that rook moves vertically" do
+    it "should check that rook moves vertically upwards" do
       r = Rook.create(x_cord: 0, y_cord: 0)
       expect(r.valid_move?(0, 7)).to eq(true)
     end
@@ -25,6 +25,21 @@ RSpec.describe Rook, type: :model do
     it "should check that rook doesn't move like a bishop" do
       r = Rook.create(x_cord: 0, y_cord: 0)
       expect(r.valid_move?(7, 7)).to eq(false)
+    end
+
+    it "should check that rook doesn't blatantly break rules" do
+      r = Rook.create(x_cord: 0, y_cord: 0)
+      expect(r.valid_move?(2, 7)).to eq(false)
+    end
+
+    it "should check that rook moves vertically downwards" do
+      r = Rook.create(x_cord: 7, y_cord: 7)
+      expect(r.valid_move?(7, 0)).to eq(true)
+    end
+
+    it "should check that rook moves horizontally to the left" do
+      r = Rook.create(x_cord: 7, y_cord: 0)
+      expect(r.valid_move?(0, 0)).to eq(true)
     end
   end
 end
