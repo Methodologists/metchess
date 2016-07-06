0c8dc2312294a2f0e05f650127bf426ac5798d1a
Aleks Real <aleksanderreal@gmail.com>
2016-07-06 23:34:34 +0000

    Switched position_exist to Piece model

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -4,10 +4,6 @@ class Rook < Piece
     position_exist?(new_x, new_y) && passes_rook_rules?(new_x, new_y)
   end
 
-  def position_exist?(x, y)
-    x >= 0 && x <= 7 && y >= 0 && y <= 7
-  end
-
   def passes_rook_rules?(x, y)
     if self.x_cord == x || self.y_cord == y
       return true
