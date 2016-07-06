9448996a9c798b3450b35bb7209241f4cc6f7daf
Aleks Real <aleksanderreal@gmail.com>
2016-07-06 21:39:06 +0000

    Added all things pawn valid_move related

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -4,40 +4,72 @@ class Pawn < Piece
     on_board?(new_x, new_y) && passes_pawn_rules?(new_x, new_y)
   end
 
-  #we need to create a method that makes sure the pawn moves according to its rules
   def passes_pawn_rules?(x, y)
+    if two_squares_forward?(x, y)
+      return true
+    elsif one_square_forward?(x, y)
+      return true
+    elsif diagonal_move?(x, y)
+      return true
+    end
 
+    return false
   end
 
-  #we need to create a method that makes sure the pawn is moving into a position that exists
+  def two_squares_forward?(x, y)
+    first_move_position? && not_backwards?(x, y) && (self.y_cord - y).abs == 2 && self.x_cord - x == 0
+  end
+
+  def one_square_forward?(x, y)
+    not_backwards?(x, y) && (self.y_cord - y).abs == 1 && self.x_cord - x == 0
+  end
+
+  def diagonal_move?(x, y)
+    diagonal_allowed?(x, y) && not_backwards?(x, y) && (self.y_cord - y).abs == 1 && (self.x_cord - x).abs == 1
+  end
+
+  #Checks that square is on board
   def on_board?(x, y)
     x >= 0 && x <= 7 && y >=0 && y <= 7
   end
 
-  #we need to check if its the first move for the pawn
+  #Checks that pawn is on the originating square aka first_move_position depending on its color
   def first_move_position?
-    self.y_cord == 1
+    if self.y_cord == 1 && self.color == 'white'
+      return true
+    elsif self.y_cord == 6 && self.color == 'black'
+      return true
+    else
+      return false
+    end
   end
 
-  #we need to check whether or not a diagonal move is allowed
-  #this will be incorporated into passes_pawn_rules? method
+  #Checks whether or not position pawn is moving to is backwards
+  def not_backwards?(x, y)
+    if self.color == 'white' && y > self.y_cord
+      return true
+    elsif self.color == 'black' && y < self.y_cord
+      return true
+    end
 
-  def diagonal_allowed?
-
+    return false
   end
 
-  #diagonal_allowed? should also check the color of opposition's piece color, if the piece color
-  #is the opposite color then return false
+  #Checks if diagonal move is allowed based on position it's moving to
+  def diagonal_allowed?(x, y)
+    if square_occupied?(x, y)
+      target_piece = Piece.find_by(x_cord: x, y_cord: y)
+      if self.color != target_piece.color
+        return true
+      end
+    end
 
-  def same_color?(x, y)
-
+    return false
   end
 
-  def two_jumps_allowed?(x, y)
-
+  def square_occupied?(x, y)
+    Piece.where(x_cord: x, y_cord: y).present?
   end
-
-  #should create a separate method that checks that pawn moves 
 
   def image
     if color == "white"
