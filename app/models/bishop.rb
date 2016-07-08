393fa20ece57dbd8f40de12465080077113c5d79
Ziyue <catherine000000@gmail.com>
2016-07-08 15:30:19 +0000

    WIP PR bishop_move

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,5 +1,20 @@
 class Bishop < Piece
 
+	def no_move?(x,y)
+		(x == x_cord) && (y == y_cord) ? true : false
+	end
+
+	def off_board?(x,y)
+		(x > 8 || y > 8 || x < 1 || y < 1) ? true : false
+	end
+
+	def bishop_move (x,y)
+		(x - x_cord).abs == (y - y_cord).abs
+		#if obstructed diagonally
+		return false if no_move(x,y)
+		return false if off_board(x,y)
+	end
+
   def image
     if color == "white"
       "\u2657"
