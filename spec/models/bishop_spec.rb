d7a4a9916cf085e3c6f519113107b290d0c8ae1d
Ziyue <catherine000000@gmail.com>
2016-07-10 17:44:03 +0000

    starting on bishop_move_test

--- spec/models/bishop_spec.rb
+++ spec/models/bishop_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Bishop, type: :model do
+RSpec.describe Pawn, type: :model do
+
+
+  describe "valid move for Pawn piece" do
+
+    it "should return false if move is out of the board" do
+    	b = Bishop.create(x_cord:0, y_cord: 0)
+      expect(b.valid_move?(2,10)).to eq false
+    end
+
 
 end
