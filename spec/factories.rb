bc2d196c652ad41b8b30ac5e2347861ecb7909d0
Lindy Sinclair <lindyrp@gmail.com>
2016-07-12 03:38:18 +0000

    Fix association redundancy in piece.rb and user.rb

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,12 @@
 FactoryGirl.define do
 
-factory :user do
+  factory :user do
     sequence :email do |n|
       "dummyEmail#{n}@gmail.com"
     end
     password "secretPassword"
     password_confirmation "secretPassword"
   end
-    
+
+  
 end
