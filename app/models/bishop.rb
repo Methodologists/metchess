b3d1ea4c9d6990308fe8f10d9c1090fe41f38c77
Lin Rui <linrui143.gmail.com>
2016-07-14 00:14:21 +0000

    Add tests

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,20 +1,16 @@
 class Bishop < Piece
 
 	def valid_move?(new_x, new_y)
-		position_exist?(new_x, new_y) && bishop_allowed_moves?(new_x, new_y) && (!(diagonally_obstructed?(new_x, new_y)))		
+		position_exist?(new_x, new_y) && not_original_position?(new_x, new_y) && bishop_allowed_moves?(new_x, new_y)
 	end
-
 
 	def bishop_allowed_moves?(new_x, new_y)
-		if (new_x - x_cord).abs == (new_y - y_cord).abs
-			return true
+		(new_x - x_cord).abs == (new_y - y_cord).abs
 	end
 
-	def diagonally_obstructed?(new_x,new_y)
-		#needs to break is_obstructed method to diagonally_obstructed?(x,y), horizontally_obstructed?(x,y), and vertically_obstructed?(x,y)
-	end
-
-
+  def not_original_position?(x, y)
+    !(x_cord == x && y_cord == y)
+  end
 
   def image
     if color == "white"
--- spec/models/bishop_spec.rb
+++ spec/models/bishop_spec.rb
@@ -1,14 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe Pawn, type: :model do
+RSpec.describe Bishop, type: :model do
 
+  describe "#valid_move?" do
 
-  describe "valid move for Pawn piece" do
+      {
+        #starting at edge of board
+        [[0, 0], [1, 1]] => true,
+        [[0, 0], [0, 0]] => false, # same position
+        [[0, 0], [1, 2]] => false, # wrong logic
+        [[0, 0], [2, -1]] => false, # off-board
+        [[0, 0], [6, 6]] => true,
 
-    it "should return false if move is out of the board" do
-    	b = Bishop.create(x_cord:0, y_cord: 0)
-      expect(b.valid_move?(2,10)).to eq false
-    end
-
+      }.each do |before_and_after_coords, result|
+        it "returns #{result} when moving from #{before_and_after_coords.first} to #{before_and_after_coords.last}" do
+          bishop = Bishop.create(x_cord: before_and_after_coords.first.first, y_cord: before_and_after_coords.first.last)
+          expect(bishop.valid_move?(before_and_after_coords.last.first, before_and_after_coords.last.last)).to eq result
+        end
+      end
+  end
 
 end
