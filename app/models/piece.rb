be629bb150b8d46ed08865252ba85d21724519c7
Lin Rui <linrui143.gmail.com>
2016-07-20 04:43:16 +0000

    Unfinished try

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -8,70 +8,17 @@ class Piece < ActiveRecord::Base
   def obstructed_horizontally?(new_x, new_y)
 
     if allowed_move?(new_x, new_y) && y_cord == new_y
-      (x_cord + 1).upto(new_x - 1) do |delta_x|
-        return true if occupied?(delta_x, y_cord)
-      end
-    elsif path == 'horizontal' && x_cord > new_x
-      (x_cord - 1).downto(new_x + 1) do |delta_x|
-        return true if occupied?(delta_x, y_cord)
+      # move left
+      if x_cord > new_x
+        (x_cord - 1).downto(new_x + 1) {|delta_x| occupied?(delta_x, y_cord)}
+      elsif x_cord < new_x
+        (x_cord + 1).downto(new_x - 1) {|delta_x| occupied?(delta_x, y_cord)}
       end
     else
       return false
     end
+
   end
-
-  def horizontal_move?(x,y)
-    (x != x_cord) && (y == y_cord) ? true : false
-  end
-
-  # def obstructed_vertically?(new_x, new_y)
-  #   path = moving_direction(new_x, new_y)
-  #   if path == 'vertical' && y_cord < new_y
-  #     (y_cord + 1).upto(new_y - 1) do |delta_y|
-  #       return true if occupied?(x_cord, delta_y)
-  #     end
-  #   elsif path == 'vertical' && y_cord > new_y
-  #     (y_cord - 1).downto(new_y + 1) do |delta_y|
-  #       return true if occupied?(x_cord, delta_y)
-  #     end
-  #   end
-  #   false
-  # end
-
-  # def obstructed_diagonally?(new_x, new_y)
-  #   path = moving_direction(new_x, new_y)
-  #   # upwards on graph direction y = x
-  #   if path == 'diagonal' && x_cord < new_x && y_cord < new_y
-  #     (x_cord + 1).upto(new_x - 1) do |delta_x|
-  #       (y_cord + 1).upto(new_y - 1) do |delta_y|
-  #         return true if occupied?(delta_x, delta_y)
-  #       end
-  #     end
-  #   # downwards on graph direction y = x
-  #   elsif path == 'diagonal' && x_cord > new_x && y_cord > y
-  #     (x_cord - 1).downto(new_x + 1) do |delta_x|
-  #       (y_cord - 1).downto(new_y + 1) do |delta_y|
-  #         return true if occupied(delta_x, delta_y)
-  #       end
-  #     end
-  #   # upwards on graph direction y = -x
-  #   elsif path == 'diagonal' && x_cord > new_x && y_cord < new_y
-  #     (x_cord - 1).downto(new_x + 1) do |delta_x|
-  #       (y_cord + 1).upto(new_y - 1) do |delta_y|
-  #         return true if occupied?(delta_x, delta_y)
-  #       end
-  #     end
-  #   # downwards on graph direction y = -x
-  #   elsif path == 'diagonal' && x_cord < new_x && y_cord > new_y
-  #     (x_cord + 1).upto(new_x - 1) do |delta_x|
-  #       (y_cord - 1).downto(new_y + 1) do |delta_y|
-  #         return true if occupied?(delta_x, delta_y)
-  #       end
-  #     end
-  #   else
-  #   return  false
-  #   end
-  # end
 
   def occupied?(new_x, new_y)
     Piece.where(x_cord: new_x, y_cord: new_y, game_id: id).present?
