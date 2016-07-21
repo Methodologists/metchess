ce3e0ecda63e4c88dd74fcee83f5100f3d76dd60
Lin Rui <linrui143.gmail.com>
2016-07-21 07:13:39 +0000

    fixes broken tests

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -4,16 +4,19 @@ RSpec.describe Piece, type: :model do
   describe "#move_to!" do
     context "when another piece is on the new spot" do
       it "captures it by updating its coords to nil" do
-        piece1 = Piece.create(x_cord: 0, y_cord: 0, color: "white", game_id: 1)
-        piece2 = Piece.create(x_cord: 1, y_cord: 1, color: "black", game_id: 1)
-        piece1.move_to!(1, 1)
-        piece1.reload
-        piece2.reload
-        expect(piece2.x_cord).to be_nil
-        expect(piece2.y_cord).to be_nil
+        game1 = Game.create(id: 1)
+        black_pawn = Piece.find_by(type:"Pawn", color: "black", x_cord: 2, y_cord: 6)
+        black_pawn.update(x_cord: 2, y_cord: 2)
+        white_pawn = Piece.find_by(type:"Pawn", color: "white", x_cord: 2, y_cord: 1)
+        white_pawn.move_to!(2, 2)
+        black_pawn.reload
+        white_pawn.reload
+        expect(black_pawn.x_cord).to be_nil
+        expect(black_pawn.y_cord).to be_nil
       end
 
       it "does not update own coords if other piece is a friend" do
+        game1 = Game.create(id: 1)
         piece1 = Piece.create(x_cord: 0, y_cord: 0, color: "white", game_id: 1)
         piece2 = Piece.create(x_cord: 1, y_cord: 1, color: "white", game_id: 1)
         piece1.move_to!(1, 1)        
@@ -30,6 +33,8 @@ RSpec.describe Piece, type: :model do
       end
 
       it "does not update piece cords in other games" do
+        game1 = Game.create(id: 1)
+        game2 = Game.create(id: 2)
         piece1 = Piece.create(x_cord: 0, y_cord: 0, color: "white", game_id: 1)
         piece2 = Piece.create(x_cord: 1, y_cord: 1, color: "black", game_id: 2)
         piece1.move_to!(1, 1)
@@ -42,7 +47,8 @@ RSpec.describe Piece, type: :model do
 
     context "when no piece on the new spot" do
       it "updates the x and y coords of the piece" do
-        piece = Piece.create(x_cord: 0, y_cord: 0)
+        game1 = Game.create(id: 1)
+        piece = Piece.create(x_cord: 0, y_cord: 0, game_id: 1)
         piece.move_to!(1, 1)
         expect(piece.x_cord).to eq 1
         expect(piece.y_cord).to eq 1
