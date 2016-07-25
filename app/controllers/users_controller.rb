d80bcc54f5d2c02ff389709deb72166dc26d2dc3
Joshua Varghese <joshuavarghese527@gmail.com>
2016-07-25 19:32:43 +0000

    fixed some syntax errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
   def index
     @users = User.order('created_at DESC').paginate(page: params[:page], per_page: 30)
   end
-end
 
   def show
     @games = Game.all
