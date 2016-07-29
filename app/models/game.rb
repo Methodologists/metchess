26c7745f58e3dce1de173ac09ef836a20ea359cb
Aleks Real <aleksanderreal@gmail.com>
2016-07-29 21:06:19 +0000

    Finished checkmate logic

--- app/models/game.rb
+++ app/models/game.rb
@@ -18,21 +18,11 @@ class Game < ActiveRecord::Base
 
   def make_pawns!
     8.times do |n|
-        Pawn.create(
-            game_id: id, 
-            color: 'white', 
-            x_cord: n,
-            y_cord: 1
-            )
+        Pawn.create(game_id: id, color: 'white', x_cord: n, y_cord: 1)
     end
-    #
+    
     8.times do |n|
-        Pawn.create(
-            game_id: id, 
-            color: 'black', 
-            x_cord: n,
-            y_cord: 6
-            )
+        Pawn.create(game_id: id, color: 'black', x_cord: n, y_cord: 6)
     end
   end
 
@@ -101,20 +91,23 @@ class Game < ActiveRecord::Base
   end
 
   def checkmate?
-    return false if move_out_of_check? || obstruct_out_of_check? #|| method3?
-    return true if check?
+    if check?
+      return false if move_out_of_check? || obstruct_out_of_check? || capture_by_other_piece?
+    else
+      return true
+    end
   end
 
   def move_out_of_check?
     king_allowed_moves = 
-    [ [check_x, check_y + 1],
-      [check_x, check_y - 1],
-      [check_x + 1, check_y + 1],
-      [check_x + 1, check_y],
-      [check_x + 1, check_y - 1],
-      [check_x - 1, check_y + 1],
-      [check_x - 1, check_y],
-      [check_x - 1, check_y - 1]
+    [ [king_x, king_y + 1],
+      [king_x, king_y - 1],
+      [king_x + 1, king_y + 1],
+      [king_x + 1, king_y],
+      [king_x + 1, king_y - 1],
+      [king_x - 1, king_y + 1],
+      [king_x - 1, king_y],
+      [king_x - 1, king_y - 1]
       ]
     king_allowed_moves.each do |coord|
       return true if king_in_check.valid_move?(coord.first, coord.last)
@@ -125,62 +118,74 @@ class Game < ActiveRecord::Base
   def obstruct_out_of_check? #checks if there's any piece that can obstruct the path of the piece putting in check
     obstructable_positions = []
     if king_in_check.y_cord == check_piece.y_cord && king_in_check.x_cord > check_piece.x_cord
-      (check_piece.x_cord).upto(king_in_check.x_cord) do |x|
+      (check_piece.x_cord + 1).upto(king_in_check.x_cord - 1) do |x|
         obstructable_positions << [x, king_in_check.y_cord]
       end
     elsif king_in_check.y_cord == check_piece.y_cord && king_in_check.x_cord < check_piece.x_cord
-      (king_in_check.x_cord).upto(check_piece.x_cord) do |x|
+      (king_in_check.x_cord + 1).upto(check_piece.x_cord - 1) do |x|
         obstructable_positions << [x, king_in_check.y_cord]
       end
     elsif king_in_check.x_cord == check_piece.x_cord && king_in_check.y_cord > check_piece.y_cord
-      (check_piece.y_cord).upto(king_in_check.y_cord) do |y|
+      (check_piece.y_cord + 1).upto(king_in_check.y_cord - 1) do |y|
         obstructable_positions << [king_in_check.x_cord, y]
       end
     elsif king_in_check.x_cord == check_piece.x_cord && king_in_check.y_cord < check_piece.y_cord
-      (king_in_check.y_cord).upto(check_piece.y_cord) do |y|
+      (king_in_check.y_cord + 1).upto(check_piece.y_cord - 1) do |y|
         obstructable_positions << [king_in_check.x_cord, y]
       end
     #diagonally upper right  
-    elsif (king_in_check.x_cord > check_piece.x_cord) &&
-     (king_in_check.x_cord - check_piece.x_cord) == (king_in_check.y_cord - check_piece.x_cord)
-      (check_piece.x_cord).upto(king_in_check.x_cord) do |x|
-        (check_piece.y_cord).upto(king_in_check.y_cord) do |y|
-          obstructable_positions << [x, y] if (x - check_piece.x_cord == y) - (check_piece.y_cord)
+    elsif (king_in_check.x_cord > check_piece.x_cord) && (king_in_check.y_cord > check_piece.y_cord)
+      (check_piece.x_cord + 1).upto(king_in_check.x_cord - 1) do |x|
+        (check_piece.y_cord + 1).upto(king_in_check.y_cord - 1) do |y|
+          obstructable_positions << [x, y] if (x - check_piece.x_cord).abs == (y - check_piece.y_cord).abs
+        end
+      end
+    #diagonally upper left
+    elsif (king_in_check.x_cord < check_piece.x_cord) && (king_in_check.y_cord > check_piece.y_cord)
+      (king_in_check.x_cord + 1).upto(check_piece.x_cord - 1) do |x|
+        (check_piece.y_cord + 1).upto(king_in_check.y_cord - 1) do |y|
+          obstructable_positions << [x, y] if (x - king_in_check.x_cord).abs == (y - check_piece.y_cord).abs
+        end
+      end
+    #diagonally lower left
+    elsif (king_in_check.x_cord < check_piece.x_cord) && (king_in_check.y_cord < check_piece.y_cord)
+      (king_in_check.x_cord + 1).upto(check_piece.x_cord - 1) do |x|
+        (king_in_check.y_cord + 1).upto(check_piece.y_cord - 1) do |y|
+          obstructable_positions << [x, y] if (x - king_in_check.x_cord).abs == (y - king_in_check.y_cord).abs
+        end
+      end
+    #diagonally lower right
+    elsif (king_in_check.x_cord > check_piece.x_cord) && (king_in_check.y_cord < check_piece.y_cord)
+      (check_piece.x_cord + 1).upto(king_in_check - 1) do |x|
+        (king_in_check.y_cord + 1).upto(check_piece - 1) do |y|
+          obstructable_positions << [x, y] if (x - check_piece.x_cord).abs == (y - check_piece.y_cord).abs
         end
       end
     end
-    #this is going to be very similar to the is_obstructed logic
-    #we are going to find every situation where the king can be in check
-    #this method will only apply horizontally and diagonally
-    #we will loop through each of our pieces and see if any of them can make a valid move into the path of
-    #the check_piece
-    #so let's break this down into micro steps...
-    #first we need to find the check piece (check_pieces)
-    #then we need to find the positions that make up the path from the check_piece to the king in check
-    #we need to do this for every possible scenario
-    # => horizontal to the right, horizontal to the left
-    # => vertically upwards, vertically downwards
-    # => diagonally upper right, diagonally lower right, diagonally upper left, diagonally lower left
-    #whenever an obstructable position fits the description, we put it into an array obstructable_positions
-    #then we need to loop through each of our pieces and see which ones are allowed to move to these positions
-    #if any of our pieces fits this description then return true for method
-    #if after looping through everything, there is no piece that can obstruct the path, return false
-    #block_pieces << Pieces.where(game_id: id, color: current_turn).where.not(x_cord: nil, y_cord: nil)
 
+    friendly_pieces = Piece.where(game_id: id, color: current_turn)
+    friendly_pieces.each do |piece|
+      obstructable_positions.each do |position|
+        return true if piece.valid_move?(position[0], position[1])
+      end
+    end
+
+    false
   end
 
-  # def method3? #checks if there's a piece that can capture the piece that's putting the king in check
-  #   pieces.each do |piece|
-  #     return true if piece.valid_move?(check_piece.x_cord, check_piece.y_cord)
-  #   end
+  def capture_by_other_piece?
+    friendly_pieces = Piece.where(game_id: id, color: current_turn)
+    friendly_pieces.each do |piece|
+      return true if piece.valid_move?(check_piece.x_cord, check_piece.y_cord)
+    end
 
-  #   return false
-  # end
+    false
+  end
 
   def check_piece
     check_piece = []
     Piece.where(game_id: id).where.not(color: current_turn).each do |piece|
-      check_piece << piece.valid_move?(check_x, check_y)
+      check_piece << piece.valid_move?(king_x, king_y)
     end
     check_piece
   end
@@ -190,12 +195,12 @@ class Game < ActiveRecord::Base
     king
   end
 
-  def check_x
+  def king_x
     x = king_in_check.x_cord
     x
   end
 
-  def check_y
+  def king_y
     y = king_in_check.y_cord
     y
   end
